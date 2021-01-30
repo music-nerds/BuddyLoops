3bcc923c702b615d15ece323e31877c60dc71c45
Joshua Geisler <joshuageisler@Joshuas-MacBook-Pro.local>
2021-01-30 08:57:00 -0500

    faster touch parsing code

--- src/components/monoSynthArp.tsx
+++ src/components/monoSynthArp.tsx
@@ -8,16 +8,15 @@ export interface MonoSynthArpProps {
 }
 
 interface TouchProps {
-  index: number;
-  identifier: number;
+  [identifier: number]: number;
 }
 
 const MonoSynthArp: React.FC<MonoSynthArpProps> = ({ synth }) => {
   // const socket = useContext(SocketContext);
   const { context, setContext } = useContext(ReactAudioContext);
   const [hold, setHold] = useState(false);
-  const touches: React.MutableRefObject<TouchProps[]> = useRef([]);
-  const holdNotes: React.MutableRefObject<TouchProps[]> = useRef([]);
+  const touches: React.MutableRefObject<TouchProps> = useRef({});
+  const holdNotes: React.MutableRefObject<TouchProps> = useRef({});
   const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
     e.preventDefault();
     const loc = e.changedTouches[0];
@@ -27,29 +26,15 @@ const MonoSynthArp: React.FC<MonoSynthArpProps> = ({ synth }) => {
     ) as HTMLDivElement;
     const index = Number(location.dataset.index);
     if (index >= 0) {
-      if (!hold) {
-        touches.current.push({
-          identifier: loc.identifier,
-          index,
-        });
-      } else {
-        if (!touches.current.some((t) => t.index === index)) {
-          touches.current.push({
-            identifier: loc.identifier,
-            index,
-          });
-        } else {
-          touches.current = touches.current.filter((t) => t.index !== index);
-        }
-        holdNotes.current = [...touches.current];
-      }
+      touches.current[loc.identifier] = index;
+      if (hold) holdNotes.current = { ...touches.current };
     }
     // error handling for stuck notes
     // if finger triggers device-level events,
     // app touch events won't fire as normal.
     // touching synth again will overwrite touches array
-    if (!hold && touches.current.length > e.touches.length) {
-      touches.current = [];
+    if (!hold && Object.keys(touches.current).length > e.touches.length) {
+      touches.current = {};
       for (let i = 0; i < e.touches.length; i++) {
         const loc = e.touches[i];
         const location = document.elementFromPoint(
@@ -58,14 +43,11 @@ const MonoSynthArp: React.FC<MonoSynthArpProps> = ({ synth }) => {
         ) as HTMLDivElement;
         const index = Number(location.dataset.index);
         if (index >= 0) {
-          touches.current.push({
-            identifier: loc.identifier,
-            index,
-          });
+          touches.current[loc.identifier] = index;
         }
       }
     }
-    synth.arpNotes = touches.current.map((t) => t.index);
+    synth.arpNotes = Object.values(touches.current);
     if (!context.isPlaying) {
       setContext({ ...context });
     }
@@ -74,10 +56,8 @@ const MonoSynthArp: React.FC<MonoSynthArpProps> = ({ synth }) => {
     e.preventDefault();
     const loc = e.changedTouches[0];
     if (!hold) {
-      touches.current = touches.current.filter(
-        (t) => t.identifier !== loc.identifier
-      );
-      synth.arpNotes = touches.current.map((t) => t.index);
+      delete touches.current[loc.identifier];
+      synth.arpNotes = Object.values(touches.current);
     }
     if (!context.isPlaying) {
       setContext({ ...context });
@@ -93,14 +73,11 @@ const MonoSynthArp: React.FC<MonoSynthArpProps> = ({ synth }) => {
     let index = Number(location.dataset.index);
     if (index >= 0) {
       if (!hold) {
-        const thisTouch = touches.current.find(
-          (t) => t.identifier === loc.identifier
-        )!;
-        thisTouch.index = index;
+        touches.current[loc.identifier] = index;
       } else {
-        holdNotes.current = [...touches.current];
+        holdNotes.current = { ...touches.current };
       }
-      synth.arpNotes = touches.current.map((t) => t.index);
+      synth.arpNotes = Object.values(touches.current);
       if (!context.isPlaying) {
         setContext({ ...context });
       }
@@ -114,7 +91,7 @@ const MonoSynthArp: React.FC<MonoSynthArpProps> = ({ synth }) => {
       synth.arpIndex = 0;
     } else {
       setHold(true);
-      synth.arpNotes = holdNotes.current.map((t) => t.index);
+      synth.arpNotes = Object.values(holdNotes.current).map((t) => t.index);
     }
   };
   return (
