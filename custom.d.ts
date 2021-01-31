dab875c6968ebc4d7391e27515c41c64cc318f36
Joshua Geisler <joshuageisler@Joshuas-MacBook-Pro.local>
2021-01-30 21:47:36 -0500

    landing page in progress

--- custom.d.ts
+++ custom.d.ts
@@ -2,3 +2,7 @@ declare module "*.wav" {
   const content: string;
   export default content;
 }
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,42 +1,41 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom'
-import Button from '@material-ui/core/Button';
-import { ReactAudioContext, DeviceID } from '../app';
-import { v4 as uuidv4 } from 'uuid';
-import './landing.css';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import { ReactAudioContext, DeviceID } from "../app";
+import { v4 as uuidv4 } from "uuid";
+import "./landing.css";
+
+// const logo = require("../../public/images/logo.png");
 
 interface Props {
   setReady: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Landing: React.FC<Props> = ({setReady}) => {
+const Landing: React.FC<Props> = ({ setReady }) => {
   const { context } = useContext(ReactAudioContext);
   const deviceID = useContext(DeviceID);
   const handleStart = () => {
     // if you're hitting this button, you're the host
     context.hostID = deviceID;
-    if(context.context.state !== 'running'){
-      context.context.resume()
-        .then(() => {
-          // triggers re-render to avoid context overlay
-          setReady(true);
-        })
+    if (context.context.state !== "running") {
+      context.context.resume().then(() => {
+        // triggers re-render to avoid context overlay
+        setReady(true);
+      });
     }
-  }
+  };
 
   return (
     <div className="landing">
+      <img src="/images/logo.png" alt="Buddy Loops Logo" className="logo" />
+      <h1 className="tagline">Make Music in Real Time</h1>
       <Link to={uuidv4()}>
-        <Button 
-          id="sessionStart"
-          variant="outlined"
-          onClick={handleStart}
-        >
+        <Button id="sessionStart" variant="outlined" onClick={handleStart}>
           Get Started
         </Button>
       </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Landing;
