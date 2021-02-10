f740ddda0cfa5ba5123708b8dd7f21e0fedb5700
Joshua Geisler <joshuageisler@Joshuas-MBP.home>
2021-02-10 09:24:49 -0500

    refactored and more gradients

--- src/components/synth.tsx
+++ src/components/synth.tsx
@@ -7,14 +7,13 @@ import React, {
 } from "react";
 import { useHistory } from "react-router-dom";
 import { SocketContext, ReactAudioContext } from "../app";
+import { MonoSynth } from "../audio/synth";
 import MonoSynthControls from "./monoSynthControls";
 import MonoSynthSquares from "./monoSynthSquares";
 import MonoSynthArp from "./monoSynthArp";
-import XY from "./xy";
+import SynthEFX from "./synthEFX";
+import SynthTabs from "./synthTabs";
 import "./synth.css";
-import { MonoSynth } from "../audio/synth";
-import GridOnIcon from "@material-ui/icons/GridOn";
-import MusicNoteIcon from "@material-ui/icons/MusicNote";
 
 interface MonoSynthProps {
   beat: number;
@@ -52,48 +51,6 @@ const Synth: React.SFC<MonoSynthProps> = ({
     }
   }, []);
 
-  const setFilterValues = useCallback(
-    (x: number, y: number) => {
-      const freq = Math.pow(10, 1.3979400086720375 * x + 2.6020599913279625); // scales 0 - 1 logarithmically between 400 and 10000
-      const q = 12 / (y * 11 + 1); // scales 0 - 1 to 1 - 12
-      synth.filter.frequency.linearRampToValueAtTime(
-        freq,
-        synth.context.currentTime + 0.0001
-      );
-      synth.filter.Q.linearRampToValueAtTime(
-        q,
-        synth.context.currentTime + 0.0001
-      );
-      socket.emit("setFilter", socketID, x, y);
-    },
-    [socket, socketID, synth]
-  );
-  const setDelayValues = useCallback(
-    (x: number, y: number) => {
-      const feedback = x * 0.75; // limit max to .75
-      const gain = 1 - y; // invert value
-      synth.delay.feedback.gain.linearRampToValueAtTime(
-        feedback,
-        synth.context.currentTime + 0.0001
-      );
-      synth.delay.output.gain.linearRampToValueAtTime(
-        gain,
-        synth.context.currentTime + 0.0001
-      );
-      socket.emit("setDelay", socketID, x, y);
-    },
-    [socket, socketID, synth]
-  );
-  const setEnvelopeValues = useCallback(
-    (x: number, y: number) => {
-      const time = 0.95 * x + 0.05;
-      const length = 1 - 0.75 * y;
-      synth.releaseTime = time;
-      synth.noteLength = length;
-      socket.emit("setEnvelope", socketID, x, y);
-    },
-    [socket, socketID, synth]
-  );
   const handleHoldToggle = useCallback(() => {
     if (hold) {
       setHold(false);
@@ -111,28 +68,7 @@ const Synth: React.SFC<MonoSynthProps> = ({
   }, [context, hold, setHold, synth, socket, socketID, holdNotes, setContext]);
   return (
     <div id="synth" ref={synthDiv}>
-      <div className="synth-tabs">
-        <div
-          className={`synth-pattern-tab ${view === "arp" ? "selected" : ""}`}
-          onClick={() => setView("arp")}
-        >
-          <MusicNoteIcon
-            style={{ color: `${view === "arp" ? "white" : "#aaa"}` }}
-          />
-        </div>
-        <div
-          className={`synth-pattern-tab ${
-            view === "pattern" ? "selected" : ""
-          }`}
-          onClick={() => setView("pattern")}
-        >
-          <GridOnIcon
-            style={{
-              color: `${view === "pattern" ? "white" : "#aaa"}`,
-            }}
-          />
-        </div>
-      </div>
+      <SynthTabs view={view} setView={setView} />
       <MonoSynthControls synth={synth} socketID={socketID} />
       {view === "pattern" ? (
         <MonoSynthSquares synth={synth} beat={beat} />
@@ -146,36 +82,14 @@ const Synth: React.SFC<MonoSynthProps> = ({
           socketID={socketID}
         />
       )}
-      <div className="synth-effects-playground">
-        <div className="hold">
-          <div
-            className={`hold-btn ${hold ? "enabled" : ""}`}
-            onClick={handleHoldToggle}
-          >
-            HOLD
-          </div>
-        </div>
-        <div className="xy-pads">
-          <XY
-            setParamValues={setFilterValues}
-            initY={(width / 11) * (11 - (synth.filter.Q.value - 1))}
-            initX={width * (Math.log10(synth.filter.frequency.value) / 4)} // logarithmically scales value
-            name="Filter"
-          />
-          <XY
-            setParamValues={setEnvelopeValues}
-            initX={width * synth.releaseTime}
-            initY={width * synth.noteLength}
-            name="Envelope"
-          />
-          <XY
-            setParamValues={setDelayValues}
-            initY={width * (1 - synth.delay.output.gain.value)}
-            initX={synth.delay.feedback.gain.value * width}
-            name="Delay"
-          />
-        </div>
-      </div>
+      <SynthEFX
+        hold={hold}
+        handleHoldToggle={handleHoldToggle}
+        holdNotes={holdNotes}
+        width={width}
+        socketID={socketID}
+        synth={synth}
+      />
     </div>
   );
 };
